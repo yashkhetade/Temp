8281d0fd3f63a4c340933a7c52bee52152d8881e
yash <yashkhetade54@gmail.com>
2024-06-15 20:56:55 +0530

    new para addded

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
   <h1>Navbar</h1>
   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit tenetur quae porro animi totam? Corporis quasi excepturi vitae magnam, totam eligendi quod hic delectus vel obcaecati at, est officia eius nobis accusamus voluptas esse necessitatibus, voluptate laudantium? Earum aliquid ea dolores ab a sit quos rem necessitatibus voluptatem quaerat veritatis possimus mollitia perspiciatis, harum delectus neque.</p>
   
+  <h2>Next Para</h2>
   </>
   )
 }
