d0150d586301d4e7ebaadf78583429b26c07e5cf
yash <yashkhetade54@gmail.com>
2024-06-15 17:13:35 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+
+function App() {
+
+
+  return (
+    <>
+  
+   <Navbar/>
+
+   
+    </>
+  )
+}
+
+export default App
